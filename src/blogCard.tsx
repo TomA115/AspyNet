228ec2fedaffdd8a45355266e43a274bd2bf1a4e
Tom Aspinall <thomasaspy1@gmail.com>
2024-07-20 20:29:04 +0100

    added medium content with placeholder profile + style changes

--- src/blogCard.tsx
+++ src/blogCard.tsx
@@ -1,1 +1,27 @@
-export{};
+import './CSS/Blog.css'
+const BlogCard = (_post:Post) =>
+{
+    const toText = (node:string) =>{
+        let Tag = document.createElement('div');
+        Tag.innerHTML = node;
+        node = Tag.innerText;
+        return node;
+      }
+
+    return(
+        <div className="card">
+         <h2>{_post.title}</h2>
+         <img className='medium-photo' src={_post.photoLink} alt=''></img>
+         <p> {`${toText(_post.description.substring(0,4000))}...`}</p>
+         <a href={_post.link} target="_blank" rel="noopener noreferrer">Read more</a>   
+        </div>
+    );
+}
+
+export default BlogCard;
+export interface Post{
+    title:string;
+    description:string;
+    link:string;
+    photoLink:string;
+  };
