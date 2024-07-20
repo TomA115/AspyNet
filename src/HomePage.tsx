1d18e8cdd5b76c655861c9232599cdcdb67eaad8
Tom Aspinall <thomasaspy1@gmail.com>
2024-07-20 20:53:05 +0100

    updates to about me text

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -31,10 +31,10 @@ const HomePage: React.FC = () => {
                     <div className='about-header'>About me</div>
                     <div className='about-body'>
                     <p>I am a London based software developer currently working for the Bank of England. I mainly have backend experience focusing on data mastering and ETL pipelines but am always trying to learn new skills.</p>
-    <p>I have a preference for backend but am currently working on improving my frontend skills. I have experience in C#, .NET Core, Python, SQL Server, SSIS and more.</p>
-    <p>This website (more specifically my Medium page) is a place for me to post my musings on technology, personal projects, and anything I find interesting.</p>
+    <p>I have a preference for backend but am currently working on improving my frontend skills. I have experience in C#, .NET Core, Python, SQL Server, SSIS, CSS/HTML, RESTful APIs</p>
+    <p>This website (more specifically my <a href='https://medium.com/@thomasaspy1'>Medium page)</a> is a place for me to post my musings on technology, personal projects, and anything I find interesting.</p>
         
-    <p>If you found anything useful on my site, or even if you choose not to click any other link, then please click one of the links below. It could make a real difference.</p></div>
+    <p>If you scroll further or even if you choose not to click any other link, then please click one of the links below. You could make a real difference.</p></div>
                     <div className='link-container'>
                         <ImageLink imageUrl={`${process.env.PUBLIC_URL}/kidney-research.png`} linkUrl="https://www.kidneyresearchuk.org/support/" altText="Kidney Research UK" />
                         <ImageLink imageUrl={`${process.env.PUBLIC_URL}/CRUK.WEBP`} linkUrl="https://www.cancerresearchuk.org/" altText="Cancer Research UK" />
